883f931e530551c1b0ddebfd8484735e1eff5a8e
tcsr200722 <350524166@qq.com>
2024-07-24 08:56:21 +0000

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -28,6 +28,7 @@ UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "$([[ $WRT_REPO == *"lede"* ]
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
 UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
+UPDATE_PACKAGE "opentopd" "sirpdboy/sirpdboy-package" "main"
 
 UPDATE_PACKAGE "advancedplus" "VIKINGYFY/luci-app-advancedplus" "main"
 UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
